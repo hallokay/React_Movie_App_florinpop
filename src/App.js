7cb19ba3e4d543e3c38fe30d7485cd0c99332028
hallokay <hallokay044@gmail.com>
2021-07-16 19:49:07 +0900

    07-16-2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Movie from './conponents/Movie';
+// import './App.css';
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&page=1&query";
+
 
 function App() {
+const [movies, setMovies] = useState([]);
+
+useEffect(() => {
+  fetch(FEATURED_API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results)
+    })
+
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="movie_container">
+    {/* 무비스의 각각을 무비로 받아서 
+    무비 컴포넌트를 적용 */}
+    <header>
+      <input type="text" className="search" placeholder="Search..."/>
+    </header>
+
+   { movies.map(movie => (
+
+     <Movie key={movie.id} {...movie}/>
+    //  키를 꼭 줘야함 
+    // {...date} 쓰면 데이터의 각각
+   )) };
+   </div>
   );
 }
 
